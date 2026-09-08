c65bb7163609652c5bf6be44dbe1b64c3a707d7b
STEFANXELIZHA <deenishanth4@gmail.com>
2026-09-08 07:46:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("click", function () {
+    noBtn.style.position = "fixed";
+
+    const maxX = window.innerWidth - noBtn.offsetWidth;
+    const maxY = window.innerHeight - noBtn.offsetHeight;
+
+    const x = Math.random() * maxX;
+    const y = Math.random() * maxY;
+
+    noBtn.style.left = x + "px";
+    noBtn.style.top = y + "px";
+});
